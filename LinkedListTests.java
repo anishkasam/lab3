3e1e699464893eb3a921a637f69135dd802a3db8
anish <33907576+anishkasam@users.noreply.github.com>
2023-01-26 11:50:59 -0800

    add linked list tests

--- LinkedListTests.java
+++ LinkedListTests.java
@@ -3,5 +3,24 @@ import org.junit.*;
 import java.util.*;
 
 public class LinkedListTests {
-  
+    @Test
+    public void testAppend() {
+        LinkedList input = new LinkedList();
+        input.prepend(3);
+        input.prepend(2);
+        input.prepend(1);
+
+        LinkedList output = new LinkedList();
+        output.prepend(1);
+        output.append(2);
+        output.append(3);
+
+        assertEquals(input.toString(), output.toString());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testFirst() {
+        LinkedList input = new LinkedList();
+        assertEquals(null, input.first());
+    }
 }
