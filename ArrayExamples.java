5808c2257bb3dd78016d1b60f66b64134e11fe65
anish <33907576+anishkasam@users.noreply.github.com>
2023-01-26 22:55:28 -0800

    change code for lab report

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -1,7 +1,6 @@
 
 
 public class ArrayExamples {
-
   // Changes the input array to be in reversed order
   static void reverseInPlace(int[] arr) {
     for(int i = 0; i < arr.length / 2; i += 1) {
--- ArrayTests.java
+++ ArrayTests.java
@@ -2,23 +2,10 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
-	public void testReverseInPlace() {
-    int[] input1 = { 3 };
-    ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
-	}
-
   @Test
-  public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
-  }
-
-  @Test
-  public void testReversedTwo() {
-    int[] input1 = {1, 2, 3, 4};
-    assertArrayEquals(new int[]{4, 3, 2, 1}, ArrayExamples.reversed(input1));
+  public void testReverseInPlaceOne() {
+    int[] input1 = {1, 2, 2, 1};
+    assertArrayEquals(new int[]{1, 2, 2, 1}, ArrayExamples.reversed(input1));
   }
 
   @Test
